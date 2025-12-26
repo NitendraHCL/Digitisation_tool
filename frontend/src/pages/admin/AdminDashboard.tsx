cd9cacbe2bb0eabe809391e17a557d0ab7de8fed
hcluser <hcluser@hclgitlab.karexpert.com>
2025-12-26 13:03:37 +0000

    report to reports

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -431,7 +431,7 @@ const AdminDashboard: React.FC = () => {
                 <Area
                   type="monotone"
                   dataKey="total"
-                  name="Total Report"
+                  name="Total Reports"
                   stackId="1"
                   stroke="#3A5A9F"
                   strokeWidth={2}
